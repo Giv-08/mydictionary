7e2ab07bc9138ab6031c4148ae5047cb253c92f7
chutima puthachon <chutima.p8@gmail.com>
2024-05-08 19:42:58 +1000

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,19 @@ const Header = () => {
   return (
       <div>
         <div className="py-8">
-      <h1 className="text-3xl text-center font-bold underline">
-        My Dictionary
-      </h1>
-      <p>
-      Make your words meaningful
-      </p>
+          <h1 className="text-3xl text-center font-bold underline">
+            My Dictionary
+          </h1>
+          <p>
+            Make your words meaningful
+          </p>
+
+      <div>
+        <div>
+          <input className="px-4 py-2" type="text" placeholder="Search..." />
+          <button>Search</button>
+        </div>
+      </div>
       </div>
       </div>
   );
