203e41f600434d4842502b24f8d40e1e337e1a9f
chutima puthachon <chutima.p8@gmail.com>
2024-05-08 14:50:24 +1000

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+
+const Header = () => {
+  return (
+      <div>
+        <div className="py-8">
+      <h1 className="text-3xl text-center font-bold underline">
+        My Dictionary
+      </h1>
+      <p>
+      Make your words meaningful
+      </p>
+      </div>
+      </div>
+  );
+}
+
+export default Header;
