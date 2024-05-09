fe19c8e0c7467e1b93c6c04417b8b302b79ef5a3
chutima puthachon <chutima.p8@gmail.com>
2024-05-09 11:21:54 +1000

    finished header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-// import logo from './logo.svg';
-// import './App.css';
+import { createContext, useState } from "react";
 import Header from "./components/Header";
 
+// create context
+export const InputContext = createContext();
+
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const value = {
+    inputValue, setInputValue
+  }
+
   return (
+    <inputContext value={value}>
     <div className="App">
       <Header />
     </div>
+    </inputContext>
   );
 }
 
