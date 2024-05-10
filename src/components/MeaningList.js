f2c41ea9cb394a55a7b890058936d78d051ad82c
chutima puthachon <chutima.p8@gmail.com>
2024-05-10 11:17:51 +1000

    added meaninglist

---
+++ src/components/MeaningList.js
@@ -0,0 +1,9 @@
+// import {  } from "react";
+
+const MeaningList = () => {
+  return(
+  <div></div>
+);
+};
+
+export default MeaningList;
--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useContext } from "react";
+import { InputContext } from "../App";
 import axios from "axios";
+import { MeaningList } from "./MeaningList";
 
-axios.defaults.baseUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en'
+axios.defaults.baseURL = 'https://api.dictionaryapi.dev/api/v2/entries/en';
 
 const ResultList = () => {
   const { inputValue } = useContext(InputContext);
@@ -9,7 +11,7 @@ const ResultList = () => {
   const [ error, setError ] = useState("");
   const [ loading, setLoading ] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = async (param) => {
     try {
       setLoading(true);
       const res = await axios(`/${param}`);
@@ -27,14 +29,26 @@ const ResultList = () => {
       fetchData(inputValue)
     }
   }, [inputValue])
+
+  if(loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if(error) {
+    return <h2 className="text-center">No definition found</h2>
+  }
+
   return (
     <div className="container mx-auto">
+      { response && (
       <div>
         <h3 className="font-bold mt-4">Meaning:</h3>
+        <MeaningList mean={response} />
         <h3 className="font-bold mt-4">Example:</h3>
         <h3 className="font-bold mt-4">Synonym:</h3>
         <h3 className="font-bold mt-4">Antonym:</h3>
       </div>
+      )}
     </div>
   );
 
