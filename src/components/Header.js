be158881c9e88604931ada8c2569b742084636ef
chutima puthachon <chutima.p8@gmail.com>
2024-05-12 21:09:27 +1000

    tailwind header

--- src/components/Header.js
+++ src/components/Header.js
@@ -24,19 +24,19 @@ const Header = () => {
           <h1 className="text-3xl text-center font-bold">
             My Dictionary
           </h1>
-          <p className="text-center">
+          <p className="text-center py-4">
             Make your words meaningful
           </p>
 
       <div>
-        <div className="flex flex-col items-center">
+        <div className="flex justify-center">
           <input className="px-3 py-2 md:w-80 bg-gray-100 text-black " type="text" placeholder="Search..." onChange={handleInputChange} value={value} onKeyDown={handleInputKeyDown}/>
           <button className="bg-green-500 border-l text-center text-white px-3 py-2" onClick={handleSubmit}>Search</button>
         </div>
       </div>
 
       { inputValue && (
-      <h3>Result : <span className="font-bold">{inputValue}</span></h3>
+      <h3 className="py-2 text-center text-2xl">Result : <span className="font-bold">{inputValue}</span></h3>
       )}
       </div>
       </div>
